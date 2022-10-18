97414cb777763e6d67df544842dbd172bae1ddf9
REZAN <asifrezan@gmail.com>
2022-10-19 01:28:04 +0600

    create initial project

---
+++ healthos_assignment/healthos_assignment/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for healthos_assignment project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthos_assignment.settings')
+
+application = get_asgi_application()
